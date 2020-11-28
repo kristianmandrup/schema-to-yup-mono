bdfed6b1773fcdef34966b6ff2a858948b8c0458
Kristian Mandrup <kmandrup@gmail.com>
2020-11-28 23:56:03 +0100

    add tests for base type constraints

--- packages/base-type/src/__tests__/base-type-constraint.test.ts
+++ packages/base-type/src/__tests__/base-type-constraint.test.ts
@@ -6,21 +6,70 @@ const createBaseTypeConstraint = (handler, propertySchema, config) => {
 
 describe("BaseTypeConstraint", () => {
   let config = {};
-  let handler = {};
+  let handler: any = {};
   let propertySchema = {};
+  let btc;
+  beforeAll(() => {
+    btc = createBaseTypeConstraint(handler, propertySchema, config);
+  });
 
-  describe("setTypeInstance(inst)", () => {
-    let btc;
-    beforeAll(() => {
-      btc = createBaseTypeConstraint(handler, propertySchema, config);
+  context("instance", () => {
+    describe("setTypeInstance(inst)", () => {
+      it("sets typeInstance", () => {
+        const inst = {
+          x: 2,
+        };
+        btc.setTypeInstance(inst);
+        expect(btc.typeInstance).toBe(inst);
+      });
     });
 
-    it("sets typeInstance", () => {
-      const inst = {
-        x: 2,
-      };
-      btc.setTypeInstance(inst);
-      expect(btc.typeInstance).toBe(inst);
+    describe("errorHandler", () => {
+      it("returns error handles", () => {
+        expect(btc.errorHandler).toBe(handler.errorHandler);
+      });
     });
+
+    describe("yup", () => {
+      it("returns yup", () => {
+        expect(btc.yup).toBe(handler.yup);
+      });
+    });
+
+    describe("type", () => {
+      it("returns type", () => {
+        expect(btc.type).toBe(handler.type);
+      });
+    });
+
+    describe("value", () => {
+      it("returns value", () => {
+        expect(btc.value).toBe(handler.value);
+      });
+    });
+
+    describe("key", () => {
+      it("returns key", () => {
+        expect(btc.key).toBe(handler.key);
+      });
+    });
+
+    describe("format", () => {
+      it("returns format", () => {
+        expect(btc.format).toBe(handler.format);
+      });
+    });
+
+    describe("constraints", () => {
+      it("returns constraints", () => {
+        expect(btc.constraints).toBe(handler.constraints);
+      });
+    });
+
+    // constraintNameFor(...names)
+    // chain(cb)
+    // valErrMessage(msg)
+    // valErrMessageOr(...msgNames)
+    // addConstraint(name, opts = {})
   });
 });
